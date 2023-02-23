305c3ff2e1dad4e91555169799806d59ff532c6e
Dikshant Narware <104089564+dikshantnarware@users.noreply.github.com>
2023-02-24 03:37:13 +0530

    Add files via upload

---
+++ home/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, authenticate, login
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+# password for test user is Harry$$$***000
+# Create your views here.
+def index(request):
+    print(request.user)
+    if request.user.is_anonymous:
+        return redirect("/login") 
+    return render(request, 'index.html')
+
+def loginUser(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+
+        # check if user has entered correct credentials
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request, user)
+            return redirect("/")
+
+        else:
+            # No backend authenticated the credentials
+            return render(request, 'login.html')
+
+    return render(request, 'login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
+
+# Create your views here.
+def index(request):
+    context = {
+        "variable1":"Harry is great",
+        "variable2":"Rohan is great"
+    } 
+    return render(request, 'index.html', context)
+    # return HttpResponse("this is homepage")
+
+def about(request):
+    return render(request, 'about.html') 
+
+def services(request):
+    return render(request, 'services.html')
+ 
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date = datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent!')
+    return render(request, 'contact.html')
+ 
